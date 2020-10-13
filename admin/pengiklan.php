12f4ccda0715caeb7a1099bcab56dc61fe34b713
grenderoid <68299321+grenderoid@users.noreply.github.com>
2020-10-13 20:49:55 +0700

    Update pengiklan.php

--- admin/pengiklan.php
+++ admin/pengiklan.php
@@ -94,8 +94,7 @@ unset($_SESSION['id_pengiklan']);
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fa fa-user" aria-hidden="true"></i>
-                <?php $user = $_SESSION['username']; 
-                echo $user; ?>
+                <?php echo $_SESSION['username']; ?>
               </a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="#">Lihat</a>
