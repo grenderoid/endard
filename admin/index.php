e31acc21efa062766fbf69706d19d611705498d8
grenderoid <68299321+grenderoid@users.noreply.github.com>
2020-10-14 14:29:27 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -41,7 +41,8 @@ unset($_SESSION['id_pengiklan']);
         </a>
         <a href="iklan_masuk.php" class="list-group-item list-group-item-action bg-light">Iklan Masuk
           <span class="badge badge-default badge-pill"><?php
-            $result = mysqli_query($conc, "SELECT * FROM transaksi INNER JOIN user_iklan ON transaksi.id_pengiklan=user_iklan.id_pengiklan INNER JOIN tayang_iklan ON transaksi.id_tayang=tayang_iklan.id_tayang INNER JOIN iklan ON transaksi.id_iklan=iklan.id_iklan WHERE transaksi.status='Baru'");
+            $masuk = "SELECT * FROM transaksi INNER JOIN user_iklan ON transaksi.id_pengiklan=user_iklan.id_pengiklan INNER JOIN tayang_iklan ON transaksi.id_tayang=tayang_iklan.id_tayang INNER JOIN iklan ON transaksi.id_iklan=iklan.id_iklan WHERE transaksi.status='Baru'";
+            $result = mysqli_query($conc, $masuk);
             $JumlahData = mysqli_num_rows($result);
             echo $JumlahData;
             ?>
