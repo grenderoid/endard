a97c70a673dc10477721dd9118ee77ce4f24cda8
grenderoid <68299321+grenderoid@users.noreply.github.com>
2020-10-19 20:07:21 +0700

    Update conc.php

--- user/connect/conc.php
+++ user/connect/conc.php
@@ -5,9 +5,4 @@ $password = "";
 $database = "dbiklan";
 
 $conc = new mysqli($servername, $username, $password, $database);
-
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-}
 ?>
