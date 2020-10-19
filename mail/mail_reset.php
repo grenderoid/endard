7096a9f50cc79f92ba8ea000f2d7cc0e793e93cb
grenderoid <68299321+grenderoid@users.noreply.github.com>
2020-10-19 21:18:27 +0700

    Update mail_reset.php

--- mail/mail_reset.php
+++ mail/mail_reset.php
@@ -7,12 +7,10 @@ $mail->SMTPDebug = 0;
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
-//ganti dengan email dan password yang akan di gunakan sebagai email pengirim
 $mail->Username = 'untukiklanbnews@gmail.com';
 $mail->Password = 'OneOkRock23';
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
-//ganti dengan email yg akan di gunakan sebagai email pengirim
 $mail->setFrom('untukiklanbnews@gmail.com', 'Iklan Bnews');
 $mail->addAddress($_POST['email'], $_POST['username']);
 $mail->isHTML(true);
