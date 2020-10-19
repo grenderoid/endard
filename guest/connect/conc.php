e0b498ace1ff3f56cc21799ad2e2fd93862f3a17
grenderoid <68299321+grenderoid@users.noreply.github.com>
2020-10-19 20:15:03 +0700

    Update conc.php

--- guest/connect/conc.php
+++ guest/connect/conc.php
@@ -5,8 +5,4 @@ $password = "";
 $database = "dbiklan";
 
 $conc = new mysqli($servername, $username, $password, $database);
-
-if ($conc->connect_error) {
-	die("Connection failed: " . $conc->connect_error);
-}
 ?>
