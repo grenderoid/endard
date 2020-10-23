ac69ae80215fab2ab8de04b5b1d01dc403d81936
grenderoid <68299321+grenderoid@users.noreply.github.com>
2020-10-23 12:29:19 +0700

    Create mail_reset.php

---
+++ mail/mail_reset.php
@@ -0,0 +1,21 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require "vendor/autoload.php";
+$mail = new PHPMailer(true);
+$mail->SMTPDebug = 0;
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = '';
+$mail->Password = '';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+$mail->setFrom('untukiklanbnews@gmail.com', 'Iklan Bnews');
+$mail->addAddress($email, $username);
+$mail->isHTML(true);
+$mail->Subject = "(Reset Password)";
+$mail->Body = "Silahkan klik link dibawah ini untuk me-reset password anda. (Link hanya berlaku selama 24jam)
+ <a href='http://localhost/IklanBNews/resetpass.php?t=".$token."'>http://localhost/IklanBNews/resetpass.php?t=".$token."</a>  ";
+$mail->send();
+?>
